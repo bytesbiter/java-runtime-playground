95cc4fa7e7ce9335964be3f24ffce227f8279458
rzett <rzett@software-crafter.com>
2020-06-15 00:55:55 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# java-runtime-playground
+A little gimmick with Java runtime from within a running Java application. 
+The main purpose is to understand the handling of the JVM. 
+Will be extended continuously.
---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // Get the Java Runtime
+        Runtime runtime = Runtime.getRuntime();
+
+        // The amount of max memory allocated to the JVM (processed manually)
+        System.out.println(runtime.maxMemory() / 1024 / 1024 + "mb");
+        // The amount of max memory allocated to the JVM (processed by library)
+        System.out.println(FileUtils.byteCountToDisplaySize(runtime.maxMemory()));
+
+        // The amount of unused memory allocated to the JVM (processed manually)
+        System.out.println(runtime.freeMemory() / 1000 / 1000 + "mb");
+        // The amount of unused memory allocated to the JVM (processed by library)
+        System.out.println(FileUtils.byteCountToDisplaySize(runtime.freeMemory()));
+
+        // The amount of processors allocated to the JVM
+        System.out.println(runtime.availableProcessors());
+
+        // Current process running
+        System.out.println(ProcessHandle.current().pid());
+        // Execute specific string command in separate processes
+        System.out.println(runtime.exec("Command Placeholder One").pid());
+        System.out.println(runtime.exec("Command Placeholder Two").pid());
+
+        // Get current runtime version
+        System.out.println(Runtime.version());
+
+    }
+}
